c109e7209b5db1a889179dec1ab6cf56c447e1cf
STEPHEN <STEPHEN@192.168.0.9>
2021-03-23 11:43:33 +0530

    helo

---
+++ src/test/java/level/Test1000.java
@@ -0,0 +1,33 @@
+package level;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Test1000 
+{
+	public static void main(String[] args) throws Exception
+	{
+
+		RemoteWebDriver driver;
+		WebDriverManager.chromedriver().setup();
+		driver= new ChromeDriver();
+		Thread.sleep(3000);
+		driver.manage().window().maximize();
+		Thread.sleep(3000);
+		driver.get("http://www.google.com");
+		Thread.sleep(2000);
+		
+		String x= driver.getCurrentUrl();
+		if(x.startsWith("http"))
+		{
+			System.out.println("site is secured");
+		}
+		else
+		{
+			System.out.println("not secured");
+		}
+		
+	}
+}
